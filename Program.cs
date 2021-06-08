4e940d97135f491a92d8264f4ba16102c3c42ffd
Сергей Захаров <sergei86zahar@mail.ru>
2021-06-08 14:24:27 +0700

    1

--- Program.cs
+++ Program.cs
@@ -16,8 +16,8 @@ namespace TestEntity
                 User user1 = new User { Name = "Aleksandr", Age = 33, Birth = new DateTime(1999, 10, 23) };
                 User user2 = new User { Name = "Alice", Age = 26, Birth = new DateTime(2011, 11, 03) };
 
-                db.Users.AddRange(user1, user2);
-                db.SaveChanges();
+                //db.Users.AddRange(user1, user2);
+                //db.SaveChanges();
             }
             // получение данных
             using (ApplicationContext db = new ApplicationContext())
@@ -30,7 +30,7 @@ namespace TestEntity
                 }
                 //Console.ReadLine();
             }
-
+            Console.WriteLine("=========================================================================");
             using (entityContext db = new entityContext())
             {
                 var books = db.Books.ToList();
@@ -39,8 +39,60 @@ namespace TestEntity
                 {
                     Console.WriteLine($"{u.Id}.{u.Name} - {u.Author}, {u.DateCreation.ToString()}, {u.Pages.ToString()}");
                 }
-                Console.ReadLine();
+                //Console.ReadLine();
             }
+
+            // Редактирование
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // получаем первый объект
+                User user = db.Users.FirstOrDefault();
+                if (user != null)
+                {
+                    user.Name = "Bob";
+                    user.Age = 44;
+                    //обновляем объект
+                    //db.Users.Update(user);
+                    //db.SaveChanges();
+                }
+                // выводим данные после обновления
+                Console.WriteLine("\nДанные после редактирования: =============================================");
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+
+            // Удаление
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // получаем первый объект
+                User user = db.Users.FirstOrDefault();
+                if (user != null)
+                {
+                    //удаляем объект
+                    //db.Users.Remove(user);
+                    //db.SaveChanges();
+                }
+                // выводим данные после обновления
+                Console.WriteLine("\nДанные после удаления: ====================================================");
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+            // добавление данных
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                User user1 = new User { Name = "Aleksandr", Age = 33, Birth = new DateTime(1999, 10, 23) };
+                User user2 = new User { Name = "Alice", Age = 26, Birth = new DateTime(2011, 11, 03) };
+
+                //db.Users.AddRange(user1, user2);
+                //db.SaveChanges();
+            }
+            Console.ReadLine();
         }
     }
 }
